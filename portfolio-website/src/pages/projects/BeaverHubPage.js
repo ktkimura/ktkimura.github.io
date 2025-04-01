e2c18f45912e17c72c77d128def6d659d3ed5e96
Katie Kimura <kimuraka@oregonstate.edu>
2025-03-31 23:01:00 -0700

    updating bh redesign page

--- portfolio-website/src/pages/projects/BeaverHubPage.js
+++ portfolio-website/src/pages/projects/BeaverHubPage.js
@@ -4,19 +4,16 @@ const BeaverHubPage = () => {
   return (
     <>
       <section>
-        <h2>Beaverhub Redesign</h2>
-        <p>Term/Course: Fall 2024/PSY 446</p>
-        <a
-          href="https://www.figma.com/proto/hpbqhz40CtQvBMhNVitGah/PSY446-Lab-5?node-id=116-918&starting-point-node-id=116%3A918&t=nybA54bNK6TqvT9L-1"
-          className="button"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Prototype Available Here
-        </a>
+        <h1>Beaverhub Redesign</h1>
       </section>
       <section>
-        <h3>Problem Statement</h3>
+        <div className="projectTags">
+          <p className="tag">Figma</p>
+          <p className="tag">Usability Testing</p>
+        </div>
+      </section>
+      <section>
+        <h2>Project Overview</h2>
         <p>
           Beaverhub is a website created by Oregon State University (OSU) that
           acts as a “one-stop hub for tools and resources that support
@@ -44,41 +41,91 @@ const BeaverHubPage = () => {
         </p>
       </section>
       <section>
-        <h3>Project Details</h3>
-        <section>
-          <h4 id="context">Context/Constraints</h4>
-          <ul>
-            <li>Group Project (4 people)</li>
-            <ul>
-              <li>Team Members: Daniel Thien, Ellie Herber, Sydney Stupp</li>
-            </ul>
-            <li>~10 week long project</li>
-            <li>No access to Beaverhub source code</li>
-          </ul>
-        </section>
-        <section>
-          <h4 id="goals">Goals</h4>
-          <ul>
-            <li>Improve the home page's practicality</li>
-            <li>Reimagine how information is displayed</li>
-            <li>Reduce the number of page redirections</li>
-          </ul>
-        </section>
+        <h2>Timeline</h2>
+
+        <a
+          href="https://www.figma.com/proto/hpbqhz40CtQvBMhNVitGah/PSY446-Lab-5?node-id=116-918&starting-point-node-id=116%3A918&t=nybA54bNK6TqvT9L-1"
+          className="button"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Prototype Available Here
+        </a>
       </section>
       <section>
-        <h3 id="timeline">Timeline</h3>
+        <h2>Insights</h2>
+        <p>
+          Sit sunt minim consectetur aliqua Lorem aliqua et occaecat nulla
+          labore amet culpa exercitation. Adipisicing occaecat exercitation elit
+          duis est cillum ipsum tempor ut deserunt labore. Eiusmod fugiat
+          pariatur magna proident qui in aliqua. Elit ullamco laborum esse eu
+          irure sunt quis.
+        </p>
+      </section>
+    </>
+  );
+};
 
-        <div className="timeline">
+export default BeaverHubPage;
+
+// eslint-disable-next-line
+{
+  /* <div className="timeline">
           <div className="timelineContainer right">
             <div className="timelineContent">
               <h4>October 10, 2024</h4>
               <p>Identifying a Topic</p>
+              <p>
+                Given that the main idea of the term project was to redesign an
+                interface we were somewhat familiar with and had some potential
+                areas of improvement, my group immediately thought of Beaverhub
+                as we have all had to use it at one point or another and each
+                had our gripes with its interface. We found the following major
+                pain points:
+              </p>
+              <ul>
+                <li>Access information on class schedule and assignments</li>
+                <li>
+                  Track tasks to complete and access other personalized content
+                  that supports one’s educational journey
+                </li>
+                <li>
+                  Quickly find offices and resources Locate resources and mark
+                  them as favorites
+                </li>
+              </ul>
+              <p>
+                However, due to the relatively short period of time we had to
+                work on getting a fully-functional and user-tested prototype
+                done, we chose to focus on improving Beaverhub’s home/landing
+                page as it is the first thing that users see and thus should
+                have the most important information displayed in a way that can
+                be effectively interpreted.
+              </p>
             </div>
           </div>
           <div className="timelineContainer right">
             <div className="timelineContent">
               <h4>October 17, 2024</h4>
               <p>Developing User Stories</p>
+              <p>
+                To combat the fact that the group was composed of members of a
+                similar demographic, we developed two user stories to better
+                evaluate Beaverhub’s usability. Our first user, Bobby, was
+                denoted as the “average” user of Beaverhub as an 18 year-old
+                full-time business student attending OSU in-person. Our other
+                user, Susan, was denoted as a more “fringe” user of BeaverHub as
+                she is a 35-year-old part-time asynchronous Ecampus student
+                completing her pre-med path at OSU. When evaluating Beaverhub
+                through these users’ lenses, we were able to pinpoint areas of
+                improvement that we had either overlooked or hadn’t considered.
+                Ultimately, it was apparent to us that even if we try to account
+                for as many users as possible, we ultimately are limited by our
+                own experiences and perceptions, making it imperative to
+                constantly scrutinize our design choices for possible usability
+                issues or areas of improvement to accommodate as many users as
+                possible.
+              </p>
             </div>
           </div>
           <div className="timelineContainer right">
@@ -125,27 +172,12 @@ const BeaverHubPage = () => {
       </section>
 
       <section>
-        <h3 id="conclusion">Conclusion</h3>
+        <h2 id="conclusion">Conclusion</h2>
         <p>
           Sit sunt minim consectetur aliqua Lorem aliqua et occaecat nulla
           labore amet culpa exercitation. Adipisicing occaecat exercitation elit
           duis est cillum ipsum tempor ut deserunt labore. Eiusmod fugiat
           pariatur magna proident qui in aliqua. Elit ullamco laborum esse eu
           irure sunt quis.
-        </p>
-      </section>
-      <section>
-        <h3 id="futureActions">Future Actions</h3>
-        <p>
-          Sit sunt minim consectetur aliqua Lorem aliqua et occaecat nulla
-          labore amet culpa exercitation. Adipisicing occaecat exercitation elit
-          duis est cillum ipsum tempor ut deserunt labore. Eiusmod fugiat
-          pariatur magna proident qui in aliqua. Elit ullamco laborum esse eu
-          irure sunt quis.
-        </p>
-      </section>
-    </>
-  );
-};
-
-export default BeaverHubPage;
+        </p> */
+}
